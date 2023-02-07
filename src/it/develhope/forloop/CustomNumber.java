60d5409d36dccb7c1387d1f02ea4e8bf60abfcae
Alessio Limina <alessio.limina@gmail.com>
2023-02-07 18:51:15 +0100

    Exercise For Loop 04

---
+++ src/it/develhope/forloop/CustomNumber.java
@@ -0,0 +1,17 @@
+package it.develhope.forloop;
+
+import java.util.Random;
+
+public class CustomNumber {
+    private int number;
+
+    public CustomNumber(){
+        Random random = new Random();
+        this.number = random.nextInt(1,12);}
+
+    public void getMultiplicationTable(){
+        for (int i = 12; i >=1 ; i--) {
+            System.out.println(" The number " + number + " * " + i + " = " + (number * i));
+        }
+    }
+}
---
+++ src/it/develhope/forloop/Start.java
@@ -0,0 +1,15 @@
+package it.develhope.forloop;
+
+public class Start {
+    public static void main(String[] args) {
+
+        System.out.println("---------------Starting-------------------");
+
+        CustomNumber customNumber = new CustomNumber();
+        customNumber.getMultiplicationTable();
+
+        System.out.println("-------------------------------------------");
+
+    }
+}
+
